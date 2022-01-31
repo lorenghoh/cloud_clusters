d8c6aa514183b2ed4a2cb7a95dfd387431c7960c
Loren Oh <loh@eoas.ubc.ca>
2022-01-31 00:23:08 -0800

    Implement core contiguity

--- cluster_clouds.py
+++ cluster_clouds.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
 
-import xarray as xr
-import zarr as zr
-import bsddb3 as bdb
-
 import scipy.ndimage.measurements as measure
 import scipy.ndimage.morphology as morph
 
 from tqdm import tqdm
 from pathlib import Path
+from joblib import Parallel, delayed
 
 import lib.calcs
 import lib.io
@@ -38,7 +35,7 @@ def sample_conditional_field(ds, ds_e):
         ds["TABS"][:],
         ds["QV"][:] / 1e3,
         ds["QN"][:] / 1e3,
-        ds["QP"][:] / 1e3
+        ds["QP"][:] / 1e3,
     )
 
     qn = (ds["QN"] > 0).values
@@ -90,42 +87,79 @@ def cluster_clouds():
     if (src / "pq").exists == False:
         raise FileNotFoundError("pq folder not found. Ensure config.json validation")
 
+    def get_bstruct(st2d=False):
+        b_struct = morph.generate_binary_structure(3, 2)
+
+        if st2d:
+            # Remove 3D connectivity
+            b_struct[0] = 0
+            b_struct[-1] = 0
+
+        return b_struct
+
     def write_clusters(t, ds, ds_e, src):
-        bin_st = morph.generate_binary_structure(3, 2)
-
-        # Remove 3D connectivity
-        # bin_st[0] = 0
-        # bin_st[-1] = 0
-
+        # TODO: pre-process cloud field to exclude 2d cloud slices with no core
+        # We can use the fact that the core is guaranteed to be in a cloudy region
         cor_b, cld_b, rmt_b = sample_conditional_field(ds, ds_e)
 
-        df = pd.DataFrame(columns=["coord", "cid", "type"])
-        c_map, _ = measure.label((cld_b | rmt_b), structure=bin_st)
+        b_struct = get_bstruct(st2d=True)
+        cl_map, _ = measure.label((cld_b | rmt_b), structure=b_struct)
+        cl_label = cl_map.ravel()
+
+        # Contiguous core field
+        b_struct = get_bstruct(st2d=False)
+        c_map, _ = measure.label(cor_b, structure=b_struct)
         c_label = c_map.ravel()
 
         # Parse different cloud fields
+        c_cor_map = (c_map > 0) & cor_b  # Full 3D core map
+
         cld_map = (c_map > 0) & cld_b
-        cor_map = (c_map > 0) & cor_b
         rmt_map = (c_map > 0) & ~(cld_b | cor_b)
 
-        target_fields = [cld_map, cor_map, rmt_map]
-        for i, c_fld in enumerate(target_fields):
-            c_flag = c_fld.ravel()
+        def _get_df(c_label, c_map, c_type):
+            c_flag = c_map.ravel()
 
-            # Extract indices
-            c_index = np.arange(len(c_label))
-            c_index = c_index[c_flag > 0]
-            c_id = c_label[c_flag > 0]
-            c_type = np.ones(len(c_id), dtype=int) * i
+            # Extrac indices
+            c_i = np.arange(len(c_label))
+            c_i = c_i[c_flag > 0]
 
-            df_ = pd.DataFrame.from_dict(
-                {"coord": c_index, "cid": c_id, "type": c_type}
+            _df = pd.DataFrame.from_dict(
+                {
+                    "coord": c_i,
+                    "cid": c_label[c_flag > 0],
+                    "type": np.ones(len(c_i), dtype=int) * c_type,
+                }
             )
 
-            df = pd.concat([df, df_])
+            return _df
+
+        # 3D core coordinates
+        df = _get_df(c_label, c_cor_map, 1)
+
+        # 2d cloud coordinates
+        df_cl = pd.DataFrame(columns=["coord", "cid", "type"])
+        fields = [(0, cld_map), (2, rmt_map)]
+        for i, c_fld in fields:
+            df_cl = pd.concat([df_cl, _get_df(cl_label, c_fld, i)])
+
+        def _worker(g):
+            # Pick the first row and find it in the cloud field
+            # It is guaranteed to be in the cloud field DataFrame
+            _g = g.iloc[0]
+
+            _df = df_cl[(df_cl.z == _g.z) & (df_cl.y == _g.y) & (df_cl.x == _g.x)]
+            _df = _df.assign(cid=_g.cid)
+
+            return pd.concat([g, _df])
+
+        grp = df.groupby(["cid", "z"], as_index=False)
+        with Parallel(n_jobs=20) as Pr:
+            result = Pr(delayed(_worker)(g) for _, g in grp)
+            df_out = pd.concat(result, ignore_index=True)
 
         file_name = f"{src}/clusters/cloud_cluster_{t:04d}.pq"
-        df.to_parquet(file_name)
+        df_out.to_parquet(file_name)
 
         tqdm.write(f"Written {file_name}")
 
