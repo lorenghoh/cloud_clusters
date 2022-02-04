37b7a0700ba81f71b30007cafe4a365dd570eb79
Loren Oh <loh@eoas.ubc.ca>
2022-02-04 01:02:00 -0800

    Code cleanup + cloud-only clustering

--- cluster_clouds.py
+++ cluster_clouds.py
@@ -22,7 +22,7 @@ config = lib.config.read_config()
 pwd = Path(__file__).absolute().parents[0]
 
 
-def sample_conditional_field(ds, ds_e):
+def sample_conditional_field(ds):
     """
     Define conditional fields
 
@@ -30,7 +30,6 @@ def sample_conditional_field(ds, ds_e):
     ------
     cor_b: core field (QN > 0, W > 0, B > 0)
     cld_b: cloud field (QN > 0)
-    rmt_b: ramnant of core entrainment
     TODO: trc_fld: plume (tracer-dependant)
     """
     th_v = lib.calcs.theta_v(
@@ -38,18 +37,16 @@ def sample_conditional_field(ds, ds_e):
         ds["TABS"][:],
         ds["QV"][:] / 1e3,
         ds["QN"][:] / 1e3,
-        ds["QP"][:] / 1e3
+        ds["QP"][:] / 1e3,
     )
 
     qn = (ds["QN"] > 0).values
     w = ((ds["W"] + np.roll(ds["W"], 1, axis=1)) / 2 > 0).values
     buoy = (th_v > np.nanmean(th_v, axis=(1, 2))[:, None, None]).values
-    ent = (ds_e["ETETCOR"] > 0).values
 
     # Boolean maps
     cld_b = qn
     cor_b = w & buoy & cld_b
-    rmt_b = ent
 
     # Define plume based on tracer fields (computationally intensive)
     # tr_field = ds['TR01'][:]
@@ -60,7 +57,49 @@ def sample_conditional_field(ds, ds_e):
     # c2_fld = (tr_field > \
     #             np.max(np.array([tr_ave + tr_std, tr_min]), 0)[:, None, None])
 
-    return cor_b, cld_b, rmt_b
+    return cor_b, cld_b
+
+
+def get_bstruct(st2d=False):
+    b_struct = morph.generate_binary_structure(3, 2)
+
+    if st2d:
+        # Remove 3D connectivity
+        b_struct[0] = 0
+        b_struct[-1] = 0
+
+
+def get_clusters(c_lbl, c_map, c_type):
+    c_flag = c_map.ravel()
+
+    # Extract indices
+    c_index = np.arange(len(c_lbl))
+    c_index = c_index[c_flag > 0]
+    c_id = c_lbl[c_flag > 0]
+
+    df = pd.DataFrame.from_dict({"coord": c_index, "cid": c_id}).assign(type=c_type)
+
+    return df
+
+
+def write_clusters(t, ds, src):
+    cor_b, cld_b = sample_conditional_field(ds)
+
+    c_map, _ = measure.label(cld_b, structure=get_bstruct(st2d=False))
+    c_label = c_map.ravel()
+
+    # Parse different cloud fields
+    cld_map = (c_map > 0) & cld_b
+    cor_map = (c_map > 0) & cor_b
+
+    df = pd.DataFrame(columns=["coord", "cid", "type"])
+    for i, c_fld in enumerate([cld_map, cor_map]):
+        df = pd.concat([df, get_clusters(c_label, c_fld, i)])
+
+    file_name = f"{src}/clusters_cloud/cloud_cluster_{t:04d}.pq"
+    df.to_parquet(file_name)
+
+    tqdm.write(f"Written {file_name}")
 
 
 def cluster_clouds():
@@ -90,63 +129,13 @@ def cluster_clouds():
     if (src / "pq").exists == False:
         raise FileNotFoundError("pq folder not found. Ensure config.json validation")
 
-    def write_clusters(t, ds, ds_e, src):
-        def get_bstruct(st2d=False):
-            b_struct = morph.generate_binary_structure(3, 2)
-
-            if st2d:
-                # Remove 3D connectivity
-                b_struct[0] = 0
-                b_struct[-1] = 0
-
-        cor_b, cld_b, rmt_b = sample_conditional_field(ds, ds_e)
-
-        df = pd.DataFrame(columns=["coord", "cid", "type"])
-        b_struct = get_bstruct(st2d=False)
-
-        c_map, _ = measure.label((cld_b | rmt_b), structure=b_struct)
-        c_label = c_map.ravel()
-
-        # Parse different cloud fields
-        cld_map = (c_map > 0) & cld_b
-        cor_map = (c_map > 0) & cor_b
-        rmt_map = (c_map > 0) & ~(cld_b | cor_b)
-
-        target_fields = [cld_map, cor_map, rmt_map]
-        for i, c_fld in enumerate(target_fields):
-            c_flag = c_fld.ravel()
-
-            # Extract indices
-            c_index = np.arange(len(c_label))
-            c_index = c_index[c_flag > 0]
-            c_id = c_label[c_flag > 0]
-            c_type = np.ones(len(c_id), dtype=int) * i
-
-            df_ = pd.DataFrame.from_dict(
-                {"coord": c_index, "cid": c_id, "type": c_type}
-            )
-
-            df = pd.concat([df, df_])
-
-        file_name = f"{src}/clusters/cloud_cluster_{t:04d}.pq"
-        df.to_parquet(file_name)
-
-        tqdm.write(f"Written {file_name}")
-
     ds_l = sorted((src / "variables").glob("CGILS_1728*"))
-    de_l = sorted((src / "ent_core").glob("CGILS_CORE*"))
-
-    if len(ds_l) != len(de_l):
-        raise ValueError("Database integrity check failed")
 
     for i in tqdm(range(len(ds_l))):
         ds = next(next(lib.io.open_db(ds_l[i])))
-        de = next(next(lib.io.open_db(de_l[i])))
 
-        write_clusters(i, ds, de, src)
-
+        write_clusters(i, ds, src)
         ds.close()
-        de.close()
 
 
 if __name__ == "__main__":
